4ee525a223e178252350c6a9003e7dce60e50b71
mshanemc <shane.mclaughlin@salesforce.com>
2019-05-08 07:40:15 -0500

    with open target

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,4 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n EPB
 sfdx shane:data:tree:import -p data/Contact-Reservation__c-plan.json -d data/
-sfdx force:org:open
+sfdx force:org:open -r /lightning/o/Reservation__c/list?filterName=All
