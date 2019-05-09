9e7dada4c3fbb8b5dfc43c1b7b74e5f14d1dbaac
mshanemc <shane.mclaughlin@salesforce.com>
2019-05-09 15:19:40 -0500

    1 day

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n EPB
 sfdx shane:data:tree:import -p data/Contact-Reservation__c-plan.json -d data/
