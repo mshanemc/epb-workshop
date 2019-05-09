46da5249343a3ebbf82a77e7e17f5cf295b0472a
mshanemc <shane.mclaughlin@salesforce.com>
2019-05-09 09:13:42 -0500

    add set password features

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n EPB
 sfdx shane:data:tree:import -p data/Contact-Reservation__c-plan.json -d data/
-sfdx force:org:open -r /lightning/o/Reservation__c/list?filterName=All
+sfdx force:org:open -p /lightning/o/Reservation__c/list?filterName=All
